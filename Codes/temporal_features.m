33de4afe233fc7fcbeb655e57fff7b6b1e4483e7
JulieKy <jujuk33@gmail.com>
2019-07-11 16:31:42 +1000

    Periodogram MAF and GMM without functions
    Spectral features

--- Codes/temporal_features.m
+++ Codes/temporal_features.m
@@ -16,10 +16,10 @@ time_axis = (1:N)/fn;
 %% ZERO CROSSING RATE
 ZCR=sum(abs(diff(sign(x))/2))/length(x);
 
-%% DISPLAY
- figure,
- plot(time_axis, x);
- title('Temporal representation');
+% %% DISPLAY
+%  figure,
+%  plot(time_axis, x);
+%  title('Temporal representation');
  
 %% OUTPUTS   
   output_temporal_features=ZCR;
