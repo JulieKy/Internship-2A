6e8216c54bc93ae4d2355d5bdf8c3592afd7730a
Julie KY <jujuk33@gmail.com>
2019-09-04 11:50:31 +1000

    Figures report

--- Implementation/Codes/Features/spectral_features.m
+++ Implementation/Codes/Features/spectral_features.m
@@ -30,7 +30,7 @@ wind = ones(1,floor(0.125*fn)); % 125 ms
 nover = floor(length(wind)/4); % 25% overlap
 nfft = 2^(nextpow2(length(wind))-1); % nfft
 [pxx,f] = pwelch(x,wind,nover,nfft,fn); % Welch
-f = f(f<=1000); % [0-800Hz] band of interest % Fae changed it to 1000Hz
+f = f(f<=1200); % [0-12000Hz] band of interest % Fae changed it to 1000Hz
 pxx=pxx(1:length(f));
 power = 1;                  
 xdata2 = f; % used for linear regression
@@ -87,7 +87,7 @@ spower=10*log10(pxx/pxx(end));
 meanPSD=meanfreq(pxx,f); %  The mean frequency of a power spectrum
 stdPSD=sqrt(sum(pxx.*((f-meanPSD).^2))/sum(pxx));%  The std of a power spectrum
 medPSD=medfreq(pxx,f); %  The median frequency of a power spectrum
-bw = powerbw(pxx,f); % 3db bandwidth of power spectrum
+[bw,flo,fhi,power] = powerbw(pxx,f); % 3db bandwidth of power spectrum
 p25=percentilefreq(pxx,f,25); % 25 percentile (1st quartile) of spectral power
 p75=percentilefreq(pxx,f,75); % 75 percentile (3rd quartile) of spectral power
 IQR=p75-p25; % Interquartile range of spectrum
@@ -96,7 +96,11 @@ IQR=p75-p25; % Interquartile range of spectrum
 TP=bandpower(pxx,f,[100 f(end)],'psd');% total power for 100-1000Hz
 p100_200 = bandpower(pxx,f,[100 200],'psd')/TP;
 p200_400 = bandpower(pxx,f,[200 400],'psd')/TP;
-p400_800 = bandpower(pxx,f,[400 800],'psd')/TP;
+p400_600 = bandpower(pxx,f,[400 600],'psd')/TP;
+p600_800 = bandpower(pxx,f,[600 800],'psd')/TP;
+p800_1000 = bandpower(pxx,f,[800 1000],'psd')/TP;
+p1000_1200 = bandpower(pxx,f,[1000 f(end)],'psd')/TP;
+p=[p1000_1200, p800_1000, p600_800, p400_600, p200_400, p100_200]; 
 
 %% Slope of the regression line (SL) in db/octave (Notes by Fae)
 % For octave we need to convert the frequency like this (taking the base as mean frequency):  
@@ -123,7 +127,60 @@ r_square2=mdl.Rsquared.Adjusted;
 
 %% RESULT
 % Combined final output
-output_spectral_features = [meanPSD;stdPSD;medPSD;bw;p25;p75;IQR;TP;p100_200;p200_400;p400_800;spectrum_slope2;r_square2];
+%output_spectral_features = [meanPSD;stdPSD;medPSD;bw;p25;p75;IQR;TP;p100_200;p200_400;p400_800;spectrum_slope2;r_square2];
+
+
+%% DISPLAY
+
+% Mean, median and bw
+figure, 
+hax=axes;
+y_axe=1e-4;
+p1=rectangle('Position', [flo, 0, bw, power/bw], 'FaceColor',[.9 .9 .9], 'EdgeColor', [.9 .9 .9]);hold on
+p2=plot(f, pxx, 'LineWidth', 1.5); 
+p3=line([meanPSD,meanPSD],[0, y_axe], 'Color',[.8 0 0], 'LineWidth', 1.7);
+p4=line([medPSD,medPSD],[0, y_axe], 'Color',[0 0.6 0], 'LineWidth', 1.7);
+hold off
+title('\fontsize{14}Spectrum Parameters on Signal 22: Mean, Median, Power Bandwidth');
+legend('Periodogram', 'Mean Frequency', 'Median Frequency')
+xlabel('Frequency [Hz]')
+ylabel('Power')
+
+% Mean, median and bw in a logarithmic scale
+figure, 
+hax=axes;
+y_axe=1e-4;
+p1=rectangle('Position', [flo, -80, bw, (max(10*log10(pxx))+3)-min(10*log10(pxx))], 'FaceColor',[.9 .9 .9], 'EdgeColor', [.9 .9 .9]);hold on
+p2=plot(f, 10*log10(pxx), 'LineWidth', 1.5); 
+p3=line([meanPSD,meanPSD],[-80, -30], 'Color',[.8 0 0], 'LineWidth', 1.7);
+p4=line([medPSD,medPSD],[-80, -30], 'Color',[0 0.6 0], 'LineWidth', 1.7);
+hold off
+title('\fontsize{14}Spectrum Parameters on Signal 22 with logarithmic scale: Mean, Median, Power Bandwidth');
+legend('Periodogram', 'Mean Frequency', 'Median Frequency')
+xlabel('Frequency [Hz]')
+ylabel('Power')
+
+% Figure with line
+figure, 
+hax=axes;
+plot(f, pxx,'LineWidth',2);
+hold on
+f_interval=200;
+band_end=1200;
+
+% Display lines
+for n_band = 1 : 6
+    band_start=band_end-(floor(f_interval));
+    line([band_start band_start],get(hax,'YLim'), 'Color',[0 0 0]); % Vertical lines differentiating the frequency bands
+    line([band_start band_end], [p(n_band) p(n_band)], 'LineWidth',2, 'Color',[1 0 0]); % Horizontal lines representing the periodogram mean of each frequency band
+    band_end=band_start;
+end
+
+hold off
+legend('Periodogram', 'Frequency bands', 'Mean')
+title('Welch Periodogram mean for CS')
+
+
 
 end
 
